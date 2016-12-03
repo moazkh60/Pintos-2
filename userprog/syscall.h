8d0ee3d01b34ab564d849ba6b1288e02e4d0e836
Muhammad Moaz Khan <13besemmoaz@seecs.edu.pk>
2016-12-03 21:16:16 +0500

    Pintos project 2
    Operating System pintos project 2

---
+++ userprog/syscall.h
@@ -0,0 +1,22 @@
+#ifndef USERPROG_SYSCALL_H
+#define USERPROG_SYSCALL_H
+
+#include "threads/thread.h"
+
+struct process_info {
+  tid_t pid;
+  int state;
+  char name[256];
+  int exit_code;
+
+  struct list_elem elem;
+};
+
+void set_process_exitcode(tid_t pid, int exitcode) ;
+struct process_info* get_process_info(tid_t pid) ;
+void add_process_to_list(const char* name,  tid_t tid) ;
+
+
+void syscall_init (void);
+
+#endif /* userprog/syscall.h */
